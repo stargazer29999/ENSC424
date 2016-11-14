aa1d7b25ca3c6a53be1826565f84010e05add30a
shadowcat219 <k.imhof.work@gmail.com>
2016-11-14 11:02:07 -0800

    Update waldo.m

--- waldo.m
+++ waldo.m
@@ -7,11 +7,13 @@
 %Load positive samples.
 
    % load('labelingSession.mat');
-   
+  
    % load('positiveInstances.mat'); 
  tic  
    
-    load('positiveInstances1.mat'); %38 images files, 42 ROI
+   % load('positiveInstances1.mat'); %38 images files, 42 ROI
+     load('positiveInstances2.mat'); %43 images files, 87 ROI
+     load('positiveInstances3.mat'); %47 images files, 67 ROI
 %%%%%%TODO increase number of postive images%%%%
 
 %Add the image directory to the MATLAB path.
@@ -25,34 +27,21 @@
 %Train a cascade object detector called 'stopSignDetector.xml' 
 %NOTE: The command can takes 20 minutes to run.
 
-% trainCascadeObjectDetector('WaldoDetector.xml',positiveInstances,negativeFolder,'FalseAlarmRate',0.5,'NumCascadeStages',3, 'FeatureType','HOG'); %default 'FeatureType'
-% trainCascadeObjectDetector('WaldoDetector1.xml',positiveInstances1,negativeFolder,'FalseAlarmRate',0.5,'NumCascadeStages',3, 'FeatureType','HOG'); %default 'FeatureType'
-% trainCascadeObjectDetector('WaldoDetector2.xml',positiveInstances1,negativeFolder,'FalseAlarmRate',0.75,'NumCascadeStages',3,'FeatureType','HOG'); %default 'FeatureType'
-% trainCascadeObjectDetector('WaldoDetector3.xml',positiveInstances1,negativeFolder,'FalseAlarmRate',0.5,'NumCascadeStages',3,'FeatureType', 'LBP');
-% trainCascadeObjectDetector('WaldoDetector4.xml',positiveInstances1,negativeFolder,'FalseAlarmRate',0.5,'NumCascadeStages',10,'FeatureType','HOG');
-% trainCascadeObjectDetector('WaldoDetector5.xml',positiveInstances1,negativeFolder,'FalseAlarmRate',0.5,'NumCascadeStages',10,'ObjectTrainingSize', [110,70]);
-% trainCascadeObjectDetector('WaldoDetector6.xml',positiveInstances1,negativeFolder,'FalseAlarmRate',0.25,'NumCascadeStages',10,'ObjectTrainingSize', [110,70]);
-% trainCascadeObjectDetector('WaldoDetector7.xml',positiveInstances1,negativeFolder,'FalseAlarmRate',0.25,'NumCascadeStages',20,'ObjectTrainingSize', [110,70]);
- trainCascadeObjectDetector('WaldoDetector8.xml',positiveInstances1,negativeFolder,'FalseAlarmRate',0.15,'NumCascadeStages',10,'ObjectTrainingSize', [110,70], 'TruePositiveRate', 0.997);
-
-
-%%%%%%TODO try several trainCascadeObjectDetector configrutations%%%%
+% trainCascadeObjectDetector('WaldoDetector10.xml',positiveInstances1,negativeFolder,'FalseAlarmRate',0.005,'NumCascadeStages',9,'ObjectTrainingSize', [100,60], 'TruePositiveRate', 0.998); %limit of cascade stages and false alarm rate
+% trainCascadeObjectDetector('WaldoDetector11.xml',positiveInstances2,negativeFolder,'FalseAlarmRate',0.005,'NumCascadeStages',9,'ObjectTrainingSize', [70,60], 'TruePositiveRate', 0.998); 
+% trainCascadeObjectDetector('WaldoDetector12.xml',positiveInstances2,negativeFolder,'FalseAlarmRate',0.005,'NumCascadeStages',9,'ObjectTrainingSize', [100,60], 'TruePositiveRate', 0.998); 
+% trainCascadeObjectDetector('WaldoDetector13.xml',positiveInstances2,negativeFolder,'FalseAlarmRate',0.05,'NumCascadeStages',9,'ObjectTrainingSize', [100,60], 'TruePositiveRate', 0.998); 
+trainCascadeObjectDetector('WaldoDetector14.xml',positiveInstances3,negativeFolder,'FalseAlarmRate',0.005,'NumCascadeStages',9,'ObjectTrainingSize', [70,60], 'TruePositiveRate', 0.999,'FeatureType','Haar'); 
 
 
 %% Use the newly trained classifier to detect a stop sign in an image.
 % detector = vision.CascadeObjectDetector('WaldoDetector.xml');
-% detector = vision.CascadeObjectDetector('WaldoDetector1.xml');
-% detector = vision.CascadeObjectDetector('WaldoDetector2.xml');
-% detector = vision.CascadeObjectDetector('WaldoDetector3.xml');
-% detector = vision.CascadeObjectDetector('WaldoDetector5.xml');
-% detector = vision.CascadeObjectDetector('WaldoDetector6.xml');
-detector = vision.CascadeObjectDetector('WaldoDetector8.xml');
-
+detector = vision.CascadeObjectDetector('WaldoDetector14.xml');
 
 % Read the test image.
 	img = imread('WaldoTest_small.jpg');
-    %img = imread('waldo2_GeoffCrimmins.jpg'); %Bounding boxes are
-    %extremely small here
+   % img = imread('WaldoTest.jpg');
+   % img = imread('waldo2_GeoffCrimmins.jpg'); %Bounding boxes are extremely small here
 
 %Detect a stop sign.
 	bbox = step(detector,img);
@@ -63,5 +52,3 @@ detector = vision.CascadeObjectDetector('WaldoDetector8.xml');
 %Display the detected stop sign.
 	figure; imshow(detectedImg);
 toc
-
-
